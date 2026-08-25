f64a5ac103bd0e6490e55ec9a9e17fb2ca1c1516
ZhangKunze <ZhangKunze@users.noreply.github.com>
2026-08-25 14:01:59 +0800

    继续删除没用的

--- mc.sh
+++ mc.sh
@@ -1,6 +1,6 @@
 #/data/data/com.termux/files/usr/bin/bash
 export HHH="$(pwd)"
-cd /sdcard/Android/data/com.mojang.minecraft/files/games/com.mojang/resource-pack/ZhangKunze/
+cd /storage/emulated/0/Android/data/com.mojang.minecraft/files/games/com.mojang/resource_packs/ZhangKunze
 zip -r ZhangKunze.mcpack.zip *
 cp -f ZhangKunze.mcpack.zip ~/ZhangKunze.github.io/minecraft/ZhangKunze.zip
 cd $HHH
